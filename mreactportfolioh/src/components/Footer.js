161e14cb851c95a1fe8b7e75f4ceae1fa7defb7e
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-07-31 17:35:58 -0400

    quick update to home.js file

--- mreactportfolioh/src/components/Footer.js
+++ mreactportfolioh/src/components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
+      <footer class="navbar navbar-inverse navbar-fixed-bottom navbar-dark bg-dark">
         <div class="container">
-          <p class="m-0 text-center text-white">
+          <p class="navbar-text">
             Copyright &copy; MH Full-Stack Dev LLC., 2021
           </p>
         </div>
--- mreactportfolioh/src/components/Home.js
+++ mreactportfolioh/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typed from "react-typed";
+import '../App.css'
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div class="home">
       <div class="main-info">
           <h1>Full-Stack Developer</h1>
+          <h2>
           <Typed
               class="typed-text"
               strings={["HTML", "CSS", "JavaScript", "MongoDB", "mySQL", "NodeJs","ReactJS", "Full Stack", "MERN"]}
@@ -14,7 +16,9 @@ const Home = () => {
               backSpeed={60}
               loop
               /> 
+              </h2>
           </div>
+
         </div>
   )
 }
